e8bd4f61f15f41e20e153bc05e9ef79cde4b174b
hanietaheri <st.honeyeh@gmail.com>
2016-10-27 20:33:34 +0330

    compression.h

--- Data_Compression.h
+++ Data_Compression.h
@@ -1,16 +1,20 @@
 #ifndef DATA_COMPRESSION_H
 #define DATA_COMPRESSION_H
+#include<string>
+#include"Vector.h"
+#include"firstNode.h"
+
+using namespace std;
 
 
 class Data_Compression
 {
     public:
-        Data_Compression();
-        virtual ~Data_Compression();
-
-    protected:
+        void init(usi,usi,usi);
+        void readingFile();
 
     private:
+
 };
 
 #endif // DATA_COMPRESSION_H
